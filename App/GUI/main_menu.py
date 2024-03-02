823efe59507f04086ab9113543be2c442ba55439
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-02 12:47:02 -0500

    Added some code to main_menu for base app

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -9,4 +9,20 @@ import customtkinter as ctk
 # main_menu function
 def main_menu(app):
 
-    pass  # TODO: Stub
+    # Creating root window
+    root = ctk.CTk()
+    root.title("HigharcheySDL Main Menu")
+    root.geometry("850x525+200+200")
+
+    # Defining on_close function
+    def on_close():
+        root.destroy()
+        app.returnStatement = "exit"
+
+    # Assigning the buttons on the tkinter window top bar
+    root.protocol("WM_DELETE_WINDOW", on_close)
+
+    # TODO: Code goes here
+
+    # Root mainloop
+    root.mainloop()
